4686ae657904223dce87e1534df67d7d53ff428c
thewonelolf <thewonelolf@github>
2020-05-24 01:12:52 -0400

    Can be called from file or seperate program. added relative path

--- r34.py
+++ r34.py
@@ -3,9 +3,12 @@ import os
 import time
 
 #dir where all the funny little sub dirs go
-data_dir = "C:\\Users\\MemeMaster\\Desktop\\r34_bot"
+data_dir = "r34_bot"
 included_types = {".png", ".jpg", ".jpeg"}
 
+if not os.path.exists(data_dir):
+    os.mkdir(data_dir)
+    
 rule34 = rule34.Sync()
 
 #Downloads images from a tag
@@ -25,3 +28,6 @@ def getImages(tag):
         if any(ext in post for ext in included_types):
             rule34.download(post)
             time.sleep(.5)
+
+if __name__ == "__main__":
+    getImages(input("Tag: "))
