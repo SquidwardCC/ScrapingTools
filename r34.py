f4ba4d9715f015354099336d6eefb4b838e8840c
gingledoof <62526960+gingledoof@users.noreply.github.com>
2020-05-25 16:42:21 -0400

    Update r34.py

--- r34.py
+++ r34.py
@@ -1,6 +1,9 @@
 import rule34
 import os
 import time
+from tqdm import tqdm
+import PIL
+from PIL import Image
 
 #dir where all the funny little sub dirs go
 data_dir = "C:\\Users\\MemeMaster\\Desktop\\r34_bot"
@@ -10,10 +13,16 @@ rule34 = rule34.Sync()
 
 #Downloads images from a tag
 def getImages(tag):
-    print("Total Images for " + tag + " : " + str(rule34.totalImages(tag)))
+    total_images = rule34.totalImages(tag)
+    print("Total Images for " + tag + " : " + str(total_images))
+    if total_images == 0:
+        print("No images found...")
+        return -1
+
 
     posts = rule34.getImageURLS(tag, singlePage=False, randomPID=True)
-    tag_dir = data_dir + "/" + tag
+
+    tag_dir = data_dir + "/" + tag.replace("/", ".")
 
     try:
         os.mkdir(tag_dir)
@@ -21,7 +30,18 @@ def getImages(tag):
         print("Directory Already there, renard")
 
     os.chdir(tag_dir)
-    for post in posts:
+    for post in tqdm(posts):
         if any(ext in post for ext in included_types):
-            rule34.download(post)
+            name = rule34.download(post)
+            try:
+                Image.open(name)
+            except:
+                print("Downloaded file corrupted... deleting")
+                os.remove(name)
             time.sleep(.5)
+
+#tags = {"sonic", "astolfo", "astolfo_(fate)", "shuten_douji", "artoria_pendragon_(all)"}
+tags = {"mordred_(fate)", "kiyohime_(fate/grand_order)", "jeanne_d&#039;arc_(fate)", "jeanne_d&#039;arc_(fate)", "jack_the_ripper_(fate/apocrypha)"}
+
+for tag in tags:
+    getImages(tag)
